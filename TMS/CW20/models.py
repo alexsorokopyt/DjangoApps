67e9847888a460253f02fb8e06a8d1bb869dd64a
Alexander <lis9845@gmail.com>
2019-05-23 21:00:21 +0300

    requirements.txt

--- TMS/CW20/models.py
+++ TMS/CW20/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    profession = models.CharField(max_length=100)
