8dc1321c836e0453c0bfeadcc91ac19aade7fdee
Alexander <lis9845@gmail.com>
2019-05-23 14:51:11 +0300

    final hw19

--- TMS/HW19/forms.py
+++ TMS/HW19/forms.py
@@ -1,13 +1,11 @@
 from django import forms
-
-
-CITIES = ['Belarus', 'Russia', 'Ukraine', 'Latvia', 'Lithuania', 'Poland']
+from HW19.cities_with_airports import CITIES
 
 
 class OrderForm(forms.Form):
     name = forms.CharField(max_length=20)
-    lastname = forms.CharField(max_length=20)
-    departure_city = forms.ChoiceField(choices=CITIES)
-    arrival_city = forms.ChoiceField(choices=CITIES)
+    surname = forms.CharField(max_length=20)
+    departure_city = forms.CharField(widget=forms.Select(choices=CITIES))
+    arrival_city = forms.CharField(widget=forms.Select(choices=CITIES))
+    departure_date = forms.DateField(widget = forms.SelectDateWidget())
     number_of_passengers = forms.IntegerField(min_value=1, max_value=300)
-    # departure_date = forms.DateField(help_text='Enter the date of your departure', input_formats='%m/%d/%Y')
--- TMS/HW19/views.py
+++ TMS/HW19/views.py
@@ -22,21 +22,28 @@ def ticket_order(request):
         form = OrderForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
-            # name = data.get('name')
-            # lastname = data.get('lastname')
-            # departure_city = data.get('departure_city')
-            # arrival_city = data.get('arrival_city')
+            name = data.get('name')
+            surname = data.get('surname')
+            departure_city = data.get('departure_city')
+            arrival_city = data.get('arrival_city')
             number_of_passengers = data.get('number_of_passengers')
-            # departure_date = data.get('departure_date')
+            departure_date = data.get('departure_date')
             if number_of_passengers == 1:
-                cost_of_one_ticket = BASIC_COST_OF_TICKET
-                result = f'{CURRENCY}{cost_of_one_ticket}'
+                cost_of_all_tickets = BASIC_COST_OF_TICKET
             elif number_of_passengers >= 2:
                 cost_of_one_ticket = BASIC_COST_OF_TICKET * 2
                 cost_of_all_tickets = cost_of_one_ticket * number_of_passengers
-                result = f'{CURRENCY}{cost_of_all_tickets}'
-            print(result)
-            return render(request, 'tickets_main_page.html', {'form': FORM})
+            context = {
+                'name': name,
+                'surname': surname,
+                'departure_city': departure_city,
+                'arrival_city': arrival_city,
+                'number_of_passengers': number_of_passengers,
+                'summ': cost_of_all_tickets,
+                'currency': CURRENCY,
+                'departure_date': departure_date
+            }
+            return render(request, 'successful_order_page.html', context)
         else:
             errors = form.errors
             return HttpResponse(f'{errors}')
